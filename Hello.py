aa52c010991de4292136ec4e12ae713c46222087
TaeGyun Kim <kimtg0003@gmail.com>
2023-03-30 11:14:57 +0900

    [Updata] pandas

--- Hello.py
+++ Hello.py
@@ -3,3 +3,14 @@ print(a+b)
 print(a-b)
 print(a*b)
 print(a//b)
+
+import pandas as pd
+import numpy as np 
+import seaborn as sns 
+
+import os
+import matplotlib.pyplot as plt 
+
+
+%matplotlib inline
+
